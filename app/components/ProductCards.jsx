05a869cc312c08f984a95ebdd766e59219d4b23a
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-09-13 08:52:44 +0200

    replaced button texts with svg icons

--- app/components/ProductCards.jsx
+++ app/components/ProductCards.jsx
@@ -97,20 +97,36 @@ const ProductCards = ({ initialProducts, currentPage }) => {
         ))}
       </div>
       <div className="flex justify-center space-x-4 mt-8">
-        <button
-          onClick={handlePreviousPage}
-          disabled={page === 0}
-          className="bg-gray-700 text-white py-2 px-4 rounded disabled:opacity-50"
-        >
-          Previous
-        </button>
+        <div>
+            <button onClick={handlePreviousPage} disabled={page === 0} className="group" >
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    height="32" width="32" 
+                    viewBox="0 0 512 512"
+                    className="group-hover:scale-110">
+                    <path 
+                    fill="#b6c1d2" 
+                    d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/>
+                </svg>
+            </button>
+
+        </div>
+        
         <p>{page + 1}</p>
-        <button
-          onClick={handleNextPage}
-          className="bg-gray-700 text-white py-2 px-4 rounded"
-        >
-          Next
-        </button>
+        <div>
+            <button onClick={handleNextPage} className="group">
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    height="32" width="32" 
+                    viewBox="0 0 512 512"
+                    className="group-hover:scale-110">
+                    <path 
+                    fill="#b6c1d2" 
+                    d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"/>
+                </svg>
+            </button>
+        </div>
+        
       </div>
     </div>
   );
